646a01ee18d40813fad5a3f01ead2f4b1d044da0
chamchimayo <chamchi.maayo@gmail.com>
2022-10-12 11:21:59 +0900

    feat: 게시글 전체 목록 조회 API 구현 완료

--- routes/posts.js
+++ routes/posts.js
@@ -9,36 +9,25 @@ const authMiddleware = require('../middlewares/auth-middleware');
 router.post("/", authMiddleware, async (req, res) => {
     const { userId, nickname } = res.locals.user;
     const { title, content } = req.body;
-    // try {
+    try {
         const createdPosts = await Posts.create({ userId, nickname, title, content });
 
         res.json({ "message": "게시글 작성에 성공하였습니다." })
 
-    // } catch (err) {
-    //     res.status(400).json({ errorMessage: "게시글 작성에 실패했습니다." });
-    // }
+    } catch (err) {
+        res.status(400).json({ errorMessage: "게시글 작성에 실패했습니다." });
+    }
 });
 
 // 게시글 목록 조회 API
 router.get("/", async (req, res) => {
-    const posts = await Posts.find().sort({ createdAt: -1 });
-    console.log(posts);
-    res.send("ok");
     // try {
-    //     console.log("here comes");
-
-    //     const data = [];
-    //     for(let i = 0; i < posts.length; i++) {
-    //         data.push({
-    //             postId: posts[i].postId.toString(),
-    //             userId: posts[i].userId,
-    //             nickname: posts[i].nickname,
-    //             title: posts[i].title,
-    //             createdAt: posts[i].createdAt,
-    //             updatedAt: posts[i].updatedAt
-    //         });
-    //     };
-    //     res.json({ data });
+        const data = await Posts.findAll({
+            attributes: {exclude: ['content'],},
+            order: [['createdAt', 'DESC']],
+        });
+        console.log(data);
+        res.json({ data });
     // } catch (err) {
     //     res.status(400).json({ errorMessage: "게시글 목록 조회에 실패했습니다." });
     // }
